c0eaa683887ef7c66c18c9a31222f161d87d1e89
maybe91 <99maybe99@mail.ru>
2023-12-06 08:36:33 +0100

    zadanie 4

---
+++ zestaw_1/zadanie_do_wyboru_4.py
@@ -0,0 +1,34 @@
+import os
+import re
+
+
+def print_letters(*args):
+    print(' '.join(args[:5]))
+
+
+def read_my_files(katalog):
+    try:
+        if not os.path.exists(katalog):
+            raise FileNotFoundError("Nie ma takiego katalogu")
+
+        txt_plik = [file for file in os.listdir(
+            katalog) if file.endswith(".txt")]
+
+        if not txt_plik:
+            raise FileNotFoundError("W podanym katalogu nie ma plików .txt")
+
+        for nazwa_pliku in txt_plik:
+            with open(os.path.join(katalog, nazwa_pliku), 'r') as file:
+                lines = file.readlines()
+                for line in lines:
+                    litery = re.findall(r'[a-zA-Z]', line)
+                    print_letters(*litery)
+
+    except FileNotFoundError as e:
+        print(e)
+    except Exception as e:
+        print(f"Wystąpił błąd: {e}")
+
+
+# Przykładowe użycie
+read_my_files(r"C:/Users/Admin/Desktop")
